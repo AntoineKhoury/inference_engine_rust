86499d2a2dc51f9ffad129dffd101a10ed44f513
Antoine Khoury <antoine.r.a.khoury@gmail.com>
2025-10-29 12:14:00 +0100

    enh: read gguf file metada and log it

--- src/gguf_loader/loader.rs
+++ src/gguf_loader/loader.rs
@@ -13,7 +13,7 @@ pub fn read_gguf(path: &str) -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    println!("Container instantiated.");
+    info!("Container instantiated.");
     let model = match container.decode(){
         Ok(c) => {
             info!("File decoded");
@@ -25,25 +25,26 @@ pub fn read_gguf(path: &str) -> Result<(), Box<dyn std::error::Error>> {
         }
     };
 
-    println!("Model Family: {}", model.model_family());
-    println!("Number of Parameters: {}", model.model_parameters());
-    println!("File Type: {}", model.file_type());
-    println!("Number of Tensors: {}", model.num_tensor());
+    info!("Model Family: {}", model.model_family());
+    info!("Number of Parameters: {}", model.model_parameters());
+    info!("File Type: {}", model.file_type());
+    info!("Number of Tensors: {}", model.num_tensor());
     for (i ,(key,value)) in model.metadata().iter().enumerate() {
-        println!("{}: {}",key,value);
+        info!("{}: {}",key,value);
     }
 
     
     
-    for i in 0..5{
+    for i in 0..20{
         let curr_tensor = &model.tensors()[i];
-        println!("\n Tensor {}", i);
-        println!("Name: {}", curr_tensor.name);
-        println!("Size: {}", curr_tensor.size);
-        println!("Offset: {}", curr_tensor.offset);
-        println!("Kind: {}", curr_tensor.kind);
+        info!("\n");
+        info!(" Tensor {}", i);
+        info!("Name: {}", curr_tensor.name);
+        info!("Size: {}", curr_tensor.size);
+        info!("Offset: {}", curr_tensor.offset);
+        info!("Kind: {}", curr_tensor.kind);
         for j in 0..4{
-            println!("Shape {}: {}", j, curr_tensor.shape[j])
+            info!("Shape {}: {}", j, curr_tensor.shape[j])
         }
     }
 
